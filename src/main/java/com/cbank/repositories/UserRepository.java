bb60834ff19933c12851c9e080096b17a73546cc
nikitap <nikitap4.92@gmail.com>
2017-11-23 09:56:59 +0300

    Added user repository

--- src/main/java/com/cbank/repositories/UserRepository.java
+++ src/main/java/com/cbank/repositories/UserRepository.java
@@ -3,6 +3,9 @@ package com.cbank.repositories;
 import com.cbank.domain.user.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 
 public interface UserRepository extends CrudRepository<User, String> {
+    Optional<User> findByUsername(String username);
 }
