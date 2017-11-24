20387b6b518be1728133ac6582e1931dd7f9e039
nikitap <nikitap4.92@gmail.com>
2017-11-24 09:35:05 +0300

    Added statement controller

---
+++ src/main/java/com/cbank/controllers/StatementController.java
@@ -0,0 +1,32 @@
+package com.cbank.controllers;
+
+import com.cbank.services.TransactionService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Podshivalov N.A.
+ * @since 21.11.2017.
+ */
+@Slf4j
+@Controller
+@AllArgsConstructor
+@AggregationModel
+@RequestMapping("/accounts/{accountNum}/statement")
+public class StatementController {
+    private final TransactionService transactionService;
+
+    @GetMapping
+    public String movements(@PathVariable String accountNum, Model model) {
+        model.addAttribute("movements", transactionService.byAccount(accountNum));
+        return "included/movements :: movementsList";
+    }
+
+
+
+}
