6cbf521c31e18efc1076a330e4f805775e1add1d
nikitap <nikitap4.92@gmail.com>
2017-11-24 09:33:43 +0300

    There are some bug fixes in authentication provider

--- src/main/java/com/cbank/config/Sha256AuthenticationProvider.java
+++ src/main/java/com/cbank/config/Sha256AuthenticationProvider.java
@@ -6,9 +6,12 @@ import com.cbank.services.UserService;
 import lombok.AllArgsConstructor;
 import lombok.val;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
+
+import java.util.Collections;
 
 /**
  * @author Podshivalov N.A.
@@ -25,20 +28,18 @@ public class Sha256AuthenticationProvider implements AuthenticationProvider{
                 .orElseThrow(() -> new AuthenticationProcessException(auth.getPrincipal() + " wasn't found"));
 
         validate(user, auth.getCredentials().toString());
-
-        auth.setAuthenticated(true);
-        return auth;
+        return new UsernamePasswordAuthenticationToken(user, user.getPassword(), Collections.emptyList());
     }
 
     private void validate(User user, String password){
-        if(user.isEnabled()) throw new AuthenticationProcessException("Your account hasn't been confirmed");
-        if(user.isNonLocked()) throw new AuthenticationProcessException("Your account was locked. Contact to us for more information");
+        if(!user.isEnabled()) throw new AuthenticationProcessException("Your account hasn't been confirmed");
+        if(!user.isNonLocked()) throw new AuthenticationProcessException("Your account was locked. Contact to us for more information");
         val hashed = userService.hashPassword(user, password);
         if(!user.getPassword().equals(hashed)) throw new AuthenticationProcessException("Your account hasn't been confirmed");
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
-        return true;
+        return authentication == UsernamePasswordAuthenticationToken.class;
     }
 }
