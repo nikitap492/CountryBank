06bc5239e4fbdd37ab932c8d98d7c4f1baeadd12
nik <nikitap4.92@gmail.com>
2017-12-20 17:58:36 +0300

    Created credit factory

---
+++ src/main/java/com/cbank/services/impl/credit/CreditFactory.java
@@ -0,0 +1,46 @@
+package com.cbank.services.impl.credit;
+
+import com.cbank.domain.Account;
+import com.cbank.domain.credit.Credit;
+import com.cbank.domain.credit.CreditState;
+import com.cbank.domain.credit.CreditType;
+import lombok.val;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static java.math.BigDecimal.ROUND_CEILING;
+
+/**
+ * @author Podshivalov N.A.
+ * @since 20.12.2017.
+ */
+@Component
+public class CreditFactory {
+
+    public Credit create(Account account, CreditType type, BigDecimal initialAmount, Integer numOfMonths){
+        val credit = new Credit();
+        credit.setState(CreditState.OPENED);
+        credit.setInitialAmount(initialAmount);
+        credit.setAccountId(account.getId());
+        credit.setNumOfWithdraws(numOfMonths);
+        credit.setType(type);
+        credit.setMonthlySum(calculateMonthlyWithdrawAmount(type, initialAmount, numOfMonths));
+        credit.setClosedAt(LocalDateTime.now().plusMonths(numOfMonths));
+        return credit;
+    }
+
+    private BigDecimal calculateMonthlyWithdrawAmount(CreditType type, BigDecimal initialAmount, Integer numOfMonths){
+        val rate = type.getPercent();
+        val months = new BigDecimal(numOfMonths);
+        val initialPerMonth = initialAmount.divide(months, 2, ROUND_CEILING);
+        return initialPerMonth.multiply(rate).add(initialPerMonth);
+    }
+
+
+
+
+
+
+}
